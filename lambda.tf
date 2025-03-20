34a28c24ec3170390931cf80b1a8675092d62cd2
w3endigo <>
2025-03-20 14:37:54 +0100

    Add AWS Lambda configuration and deployment workflow

---
+++ lambda.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region     = "eu-west-3"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "my_nodejs_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs22.x"
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      NODE_ENV = "production"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "my-lambda-bucket"
+}
+
+resource "aws_s3_bucket_object" "lambda_zip" {
+  source = "lambda.zip"
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.lambda_function.arn
+}
